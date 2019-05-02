17ddd5c3b56332c0f8629bc73c89294c595cad26
QuietClickCode <2572417548@qq.com>
2019-05-02 16:51:20 +0800

    排除springboot-starter的日志依赖

---
+++ src/test/java/com/zjj/crawl/MyProcessor.java
@@ -0,0 +1,34 @@
+package com.zjj.crawl;
+
+import us.codecraft.webmagic.Page;
+import us.codecraft.webmagic.Site;
+import us.codecraft.webmagic.Spider;
+import us.codecraft.webmagic.pipeline.ConsolePipeline;
+import us.codecraft.webmagic.pipeline.FilePipeline;
+import us.codecraft.webmagic.processor.PageProcessor;
+
+/**
+ * 2019年3月9日22:14:19
+ */
+public class MyProcessor implements PageProcessor {
+
+    public void process(Page page) {
+        //page.addTargetRequests(page.getHtml().links().all());
+        page.addTargetRequests(page.getHtml().links().regex("https://blog.csdn.net/[a-z 0-9 -]+/article/details/[0-9]{8}").all());
+        //System.out.println(page.getHtml().xpath("//*[@id=\"mainBox\"]/main/div[1]/div/div/div[1]/h1").toString());
+        page.putField("title", page.getHtml().xpath("//*[@id=\"mainBox\"]/main/div[1]/div/div/div[1]/h1"));
+        //page.putField("content", page.getHtml().xpath("//*[@id=\"article_content\"]"));
+        page.putField("content", page.getHtml().xpath("//*[@id=\"mainBox\"]/main/div[1]"));
+
+    }
+
+    public Site getSite() {
+        return Site.me().setSleepTime(100).setRetryTimes(3);
+    }
+
+    public static void main(String[] args) {
+        Spider.create(new MyProcessor()).addUrl("https://blog.csdn.net/").addPipeline(new ConsolePipeline()).addPipeline(new FilePipeline("D:/crawldata")).run();
+    }
+}
+
+
