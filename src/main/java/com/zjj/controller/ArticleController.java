4fe647d38ea6df99eb585bcc43369ee163dc1467
QuietClickCode <2572417548@qq.com>
2019-05-08 02:14:03 +0800

    实现爬取多个网页并封装到实体返回到浏览器中,下一步,随时随地终止爬虫,并返回数据到页面

---
+++ src/main/java/com/zjj/controller/ArticleController.java
@@ -0,0 +1,33 @@
+package com.zjj.controller;
+
+import com.zjj.crawl.MyProcessors;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import us.codecraft.webmagic.Spider;
+import us.codecraft.webmagic.pipeline.ConsolePipeline;
+import us.codecraft.webmagic.pipeline.FilePipeline;
+
+/**
+ * @program: znsh
+ * @description: 文章控制器
+ * @author: zjj
+ * @create: 2019-05-08 00:17:38
+ **/
+@RestController
+public class ArticleController {
+
+    @Autowired
+    private MyProcessors myProcessors;
+
+    @RequestMapping("/article")
+    public String article() {
+        Spider spider = new Spider(myProcessors);
+        spider.addUrl("https://blog.csdn.net/zhinengxuexi/article/details/89150907","https://blog.csdn.net/valada/article/details/89382217?utm_source=feed1904261").addUrl("https://blog.csdn.net/themagickeyjianan/article/details/52386981").addPipeline(new ConsolePipeline()).addPipeline(new FilePipeline("D:/crawldata")).run();
+        System.out.println("------------" + myProcessors.haha.toString());
+        spider.close();
+
+
+        return myProcessors.haha;
+    }
+}
--- src/main/java/com/zjj/crawl/MyProcessors.java
+++ src/main/java/com/zjj/crawl/MyProcessors.java
@@ -4,44 +4,44 @@ import com.zjj.model.Article;
 import org.springframework.stereotype.Component;
 import us.codecraft.webmagic.Page;
 import us.codecraft.webmagic.Site;
-import us.codecraft.webmagic.Spider;
-import us.codecraft.webmagic.pipeline.ConsolePipeline;
-import us.codecraft.webmagic.pipeline.FilePipeline;
 import us.codecraft.webmagic.processor.PageProcessor;
 
 /**
  * 2019年3月9日22:14:19
  */
 @Component
-public class MyProcessor implements PageProcessor {
+public class MyProcessors implements PageProcessor {
 
-
-    public Article article = new Article();
-
-    @Override
+    public String haha;
+    Article article = new Article();
     public void process(Page page) {
         //page.addTargetRequests(page.getHtml().links().all());
-        page.addTargetRequests(page.getHtml().links().regex("https://blog.csdn.net/[a-z 0-9 -]+/article/details/[0-9]{8}").all());
+       // page.addTargetRequests(page.getHtml().links().regex("https://blog.csdn.net/[a-z 0-9 -]+/article/details/[0-9]{8}").all());
         //System.out.println(page.getHtml().xpath("//*[@id=\"mainBox\"]/main/div[1]/div/div/div[1]/h1").toString());
         page.putField("title", page.getHtml().xpath("//*[@id=\"mainBox\"]/main/div[1]/div/div/div[1]/h1"));
         //page.putField("content", page.getHtml().xpath("//*[@id=\"article_content\"]"));
         page.putField("content", page.getHtml().xpath("//*[@id=\"mainBox\"]/main/div[1]"));
-        if (page.getResultItems() != null) {
-            article.setContent(page.getResultItems().get("content").toString());
-            article.setTitle(page.getResultItems().get("title").toString());
-        }
+
+        article.setTitle(page.getResultItems().get("title").toString());
+        article.setContent(page.getResultItems().get("content").toString());
+        haha += article.toString();
+
+        System.out.println("=============" + haha);
+
+
 
 
     }
-    @Override
+
     public Site getSite() {
         return Site.me().setSleepTime(100).setRetryTimes(3);
     }
 
-    public static void main(String[] args) {
-        Spider.create(new MyProcessor()).addUrl("https://blog.csdn.net/").addPipeline(new ConsolePipeline()).addPipeline(new FilePipeline("D:/crawldata")).run();
+    /*public static void main(String[] args) {
+        //Spider.create(new MyProcessors()).addUrl("https://blog.csdn.net/").addPipeline(new FilePipeline("D:/crawldata")).run();
+        Spider.create(new MyProcessors()).addUrl("https://blog.csdn.net/zhinengxuexi/article/details/89150907").addPipeline(new FilePipeline("D:/crawldata")).run();
+    }*/
 
-    }
 }
 
 
